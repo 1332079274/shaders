55740eb1d69c6599b06457b773b581529e192cd4
Geese Peng <1332079274@qq.com>
2019-01-07 18:56:57 +0800

    Add files via upload
    加入相机高斯模糊脚本

---
+++ PostEffects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffects : PostEffectsBase {
+
+    [Range(0, 16)]
+    public int times = 1;
+
+	// Use this for initialization
+	void Start () {
+		Check();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture des) {
+		if (effect_enable) {
+            RenderTexture tmp1 = new RenderTexture(src.width, src.height, src.depth);
+            RenderTexture tmp2 = new RenderTexture(src.width, src.height, src.depth);
+
+            Graphics.Blit(src, tmp1);
+            for (var i = 0; i < times;++i )
+            {
+                Graphics.Blit(tmp1, tmp2, material);
+                Graphics.Blit(tmp2, tmp1);
+            }
+
+            Graphics.Blit(tmp1, des);
+            
+            tmp1.Release();
+            tmp2.Release();
+		} else {
+			Graphics.Blit (src, des);
+		}
+
+	}
+}
---
+++ PostEffectsBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof(Camera))]
+public class PostEffectsBase : MonoBehaviour {
+
+	public Shader shader;
+	public Material material;
+
+	protected bool effect_enable = false;
+
+	protected void Check() {
+		if (!SystemInfo.supportsImageEffects) {
+			effect_enable = false;
+			return;
+		}
+
+		if (!shader || !shader.isSupported || 
+			!material || material.shader != shader) {
+			effect_enable = false;
+			return;
+		}
+
+		effect_enable = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Check();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
