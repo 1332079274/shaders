a8a1fc283c710aec22efb7dbda50dfd5846735d8
Geese Peng <1332079274@qq.com>
2020-01-31 11:44:47 +0800

    屏幕特效着色器

---
+++ ScreenEffectScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenEffectScript : PostEffectsBase {
+
+	// Use this for initialization
+	void Start () {
+        Check();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture des)
+    {
+        if (effect_enable)
+        {
+            Graphics.Blit(src, des, material);
+        }
+        else
+        {
+            Graphics.Blit(src, des);
+        }
+
+    }
+}
